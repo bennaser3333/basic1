ecbaf89a50b26323e4e63f60c311ec5ee3e73614
Ali Bennaser <alibennaserofficial@gmail.com>
2024-09-26 12:41:55 +0200

    basic

---
+++ Programming_Basics/Week_1/Day_2/Core/Code Challenge Predict Arrays/Predict Arrays.js
@@ -0,0 +1,35 @@
+var contactInfo = ["Paula", "Smith", "1234 Main Street", "St. Louis", "MO", 12345];
+console.log(contactInfo);
+
+// result 1 [ 'Paula', 'Smith', '1234 Main Street', 'St. Louis', 'MO', 12345 ]
+
+
+
+
+ 
+
+var produce = ["apples", "oranges"];
+var frozen = ["broccoli", "ice cream"];
+frozen.push("hashbrowns");
+console.log(frozen);
+
+
+// result 2 [ 'broccoli', 'ice cream', 'hashbrowns' ]
+
+
+
+
+
+
+var movieLibrary = ["Bambi", "E.T.", "Toy Story"];
+movieLibrary.push("Zorro");
+movieLibrary[1] = "Beetlejuice";
+console.log(movieLibrary);
+
+
+// result 3  [ 'Bambi', 'Beetlejuice', 'Toy Story', 'Zorro' ]
+
+
+
+
+
---
+++ Programming_Basics/Week_1/Day_2/Optional/Practice Commenting/Practice Commenting.js
@@ -0,0 +1,7 @@
+// create var likeCount =0
+var likeCount = 0;
+//  plus 1 to likeCount 
+function increaseLikes() {
+    //plus 1 to likeCount  
+    likeCount = likeCount + 1;
+}
